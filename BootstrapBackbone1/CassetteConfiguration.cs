5d8b9df0bd527ef0cf6aa9c911f2c95b80a1f529
scott coates <scoarescoare@gmail.com>
2012-06-15 01:04:05 -0700

    using cassette to render assetts

--- BootstrapBackbone1/CassetteConfiguration.cs
+++ BootstrapBackbone1/CassetteConfiguration.cs
@@ -17,13 +17,15 @@ namespace BootstrapBackbone1
             // This default configuration treats each file as a separate 'bundle'.
             // In production the content will be minified, but the files are not combined.
             // So you probably want to tweak these defaults!
-            bundles.AddPerIndividualFile<StylesheetBundle>("Content");
-            bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
+            //bundles.AddPerIndividualFile<StylesheetBundle>("Content");
+            //bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
 
+            bundles.Add<StylesheetBundle>("Content/assets/css");
+            bundles.AddPerSubDirectory<ScriptBundle>("Content/assets/js");
             // To combine files, try something like this instead:
             //   bundles.Add<StylesheetBundle>("Content");
             // In production mode, all of ~/Content will be combined into a single bundle.
-            
+
             // If you want a bundle per folder, try this:
             //   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
             // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
