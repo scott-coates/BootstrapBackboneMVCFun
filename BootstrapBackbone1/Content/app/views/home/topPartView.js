1f020fc1ad4eb5eb4cbe0c7b455bbe41391377fe
scott coates <scoarescoare@gmail.com>
2012-06-15 15:29:09 -0700

    show feed

--- BootstrapBackbone1/Content/app/views/home/topPartView.js
+++ BootstrapBackbone1/Content/app/views/home/topPartView.js
@@ -1,7 +1,14 @@
 var CliqFlip = (function (cliqFlip) {
 
     cliqFlip.Mvc.App.Views.TopPartView = Backbone.Marionette.ItemView.extend({
-        template: "home-topPart"
+        template: "home-topPart",
+        events: {
+            "click .btn": "showFeed"
+        },
+
+        showFeed: function () {
+            alert('this is a feed');
+        }
     });
 
     return cliqFlip;
